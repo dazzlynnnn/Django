3b427a00c7d979d15001d95bfd9f626a12b645c3
Lynn <dazzlynnnn@gmail.com>
2021-01-08 20:24:12 +0900

    Django form study

--- first-django/second/views.py
+++ first-django/second/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
 from second.models import Post
+from .forms import PostForm
 
 
 # Create your views here.
@@ -11,3 +14,13 @@ def list(request):
     }
     return render(request, 'second/list.html', context)
 
+
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            new_item = form.save()
+        return HttpResponseRedirect('/second/list/')
+
+    form = PostForm()
+    return render(request, 'second/create.html', {'form': form})
