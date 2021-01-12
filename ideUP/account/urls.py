5ef5d5e97445bbcc0646695c13a6b14e83056771
Lynn <dazzlynnnn@gmail.com>
2021-01-12 22:13:57 +0900

    urls update

--- ideUP/account/urls.py
+++ ideUP/account/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from account import views
-from django.conf.urls import url, include
+from django.conf.urls import include
 
 urlpatterns = [
-    path('accounts/', views.account_list),
-    path('accounts/<int:pk>/', views.account),
-    path('login/', views.login),
-    path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('accounts', views.account_list),
+    path('accounts/<int:pk>', views.account),
+    path('login', views.login),
+    path('auth', include('rest_framework.urls', namespace='rest_framework'))
 ]
