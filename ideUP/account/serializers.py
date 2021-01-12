11f18c4a65de764f100f92b680500da7ba83d79c
Lynn <dazzlynnnn@gmail.com>
2021-01-12 17:37:16 +0900

    DB change to MySQL
    settings.py 수정

--- ideUP/account/serializers.py
+++ ideUP/account/serializers.py
@@ -5,4 +5,4 @@ from .models import Account
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
-        fields = ['name', 'email', 'password']
+        fields = ['id', 'name', 'email', 'password']
